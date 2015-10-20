99d333acca651273fb229ee25cecdc943d3b5653
Ben Southgate <bsouthga@gmail.com>
2015-10-20 11:32:15 -0400

    align aggregate api with mongo docs

--- test/Collection.js
+++ test/Collection.js
@@ -25,7 +25,7 @@ describe('Collection', function () {
         { name: 'Lapras', type: 'water' }
       ]);
 
-      let result = await collection.aggregate({$group: {_id: '$type'}});
+      let result = await collection.aggregate([{$group: {_id: '$type'}}]);
       expect(result).to.deep.have.members([{_id: 'water' }, {_id: 'fire'}]);
     });
   });
@@ -39,7 +39,7 @@ describe('Collection', function () {
         { name: 'Lapras', type: 'water' }
       ]);
 
-      let cursor = collection.aggregateCursor({$group: {_id: '$type'}});
+      let cursor = collection.aggregateCursor([{$group: {_id: '$type'}}]);
       expect(cursor).to.be.an.instanceof(Cursor);
       let result = await cursor.toArray();
       expect(result).to.deep.have.members([{_id: 'water' }, {_id: 'fire'}]);
